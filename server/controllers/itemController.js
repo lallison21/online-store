f716e4741c1c2ea2e71168212638b312557404db
lallison <lallison@student.21-school.ru>
2022-09-09 18:44:09 +0300

    getting devices, filtering, pagination - page output

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -1,17 +1,27 @@
 const uuid = require('uuid')
 const path = require('path')
-const {Item} = require('../models/models')
+const {Item, ItemInfo} = require('../models/models')
 const ApiError = require('../error/ApiError')
 
 class ItemController {
     async create(req, res, next) {
         try {
-            const {name, price, brandId, typeId, sizeId, info} = req.body
+            let {name, price, brandId, typeId, sizeId, info} = req.body
             const {img} = req.files
             let fileName = uuid.v4() + ".jpg"
             await img.mv(path.resolve(__dirname, '..', 'static', fileName))
-
             const item = await Item.create({name, price, brandId, typeId, sizeId, img: fileName})
+
+            if (info) {
+                info = JSON.parse(info)
+                info.forEach(i =>
+                    ItemInfo.create({
+                        title: i.title,
+                        description: i.description,
+                        itemId: item.id
+                    })
+                )
+            }
 
             return res.json(item)
         } catch (e) {
@@ -20,11 +30,40 @@ class ItemController {
     }
 
     async getAll(req, res) {
-
+        let {brandId, typeId, sizeId, limit, page} = req.query
+        page = page || 1
+        limit = limit || 9
+        let offset = page * limit - limit
+        let item;
+        if (!brandId && !typeId && !sizeId) {
+            item = await Item.findAndCountAll({limit, offset})
+        } else if (brandId && !typeId && !sizeId) {
+            item = await Item.findAndCountAll({where: {brandId}, limit, offset})
+        } else if (!brandId && typeId && !sizeId) {
+            item = await Item.findAndCountAll({where: {typeId}, limit, offset})
+        } else if (!brandId && !typeId && sizeId) {
+            item = await Item.findAndCountAll({where: {sizeId}, limit, offset})
+        } else if (brandId && typeId && !sizeId) {
+            item = await Item.findAndCountAll({where:{brandId, typeId}, limit, offset})
+        } else if (brandId && !typeId && sizeId) {
+            item = await Item.findAndCountAll({where:{brandId, sizeId}, limit, offset})
+        } else if (!brandId && typeId && sizeId) {
+            item = await Item.findAndCountAll({where:{typeId, sizeId}, limit, offset})
+        } else if (brandId && typeId && sizeId) {
+            item = await Item.findAndCountAll({where:{brandId, typeId, sizeId}, limit, offset})
+        }
+        return res.json(item)
     }
 
     async getOne(req, res) {
-
+        const {id} = req.params
+        const item = await Item.findOne(
+            {
+                where: {id},
+                include: [{model: ItemInfo, as: 'info'}]
+            },
+        )
+        return res.json(item)
     }
 }
 
