f4680d0704282766828039a3157654a3048475db
Kamil Kamalov <kamil17121998@gmail.com>
2022-09-15 12:05:27 +0300

    registration and login page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,26 +5,24 @@ import Nav from "react-bootstrap/Nav";
 import {NavLink} from "react-router-dom";
 import {SHOP_ROUTE} from "../utils/consts";
 import {Button} from "react-bootstrap";
-import Container from "react-bootstrap/Container";
 import {observer} from "mobx-react-lite";
 
 const NavBar = observer(() => {
     const {user} = useContext(Context)
+
     return (
         <Navbar bg="dark" variant="dark">
-            <Container>
-                <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>BananaShop</NavLink>
-                {user.isAuth ?
-                    <Nav className="ml-auto" style={{color: 'white'}}>
-                        <Button variant={"outline-light"}>Админ панель</Button>
-                        <Button variant={"outline-light"} className='mx-2'>Выйти</Button>
-                    </Nav>
-                    :
-                    <Nav className="ml-auto" style={{color: 'white'}}>
-                        <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
-                    </Nav>
-                }
-            </Container>
+            <NavLink className="ms-5" style={{color: 'white', textDecoration: "none"}} to={SHOP_ROUTE}>BananaShop</NavLink>
+            {user.isAuth ?
+                <Nav className="ms-auto me-5" style={{color: 'white'}}>
+                    <Button variant={"outline-light"}>Админ панель</Button>
+                    <Button variant={"outline-light"} className='mx-2'>Выйти</Button>
+                </Nav>
+                :
+                <Nav className="ms-auto me-5" style={{color: 'white'}}>
+                    <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+                </Nav>
+            }
         </Navbar>
     );
 });
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import {Button, Container, Form} from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import {NavLink, useLocation} from "react-router-dom";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
 
 const Auth = () => {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
+
     return (
-        <div>
-            AUTH
-        </div>
+        <Container
+            className="d-flex justify-content-center align-items-center"
+            style={{height: window.innerHeight - 54}}
+        >
+            <Card style={{width: 600}} className="p-5">
+                <h2 className="m-auto">
+                    {isLogin ? 'Авторизация' : 'Регистрация'}
+                </h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите ваш email..."
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите ваш password..."
+                    />
+                    <Row className="d-flex justify-content-between mt-3">
+                        {isLogin ?
+                            <div className="w-50 mt-1">
+                                Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}
+                                                       style={{textDecoration: "none"}}>Зарегистрируйся!</NavLink>
+                            </div>
+                            :
+                            <div className="w-50 mt-1">
+                                Есть аккаунт? <NavLink to={LOGIN_ROUTE}
+                                                       style={{textDecoration: "none"}}>Войдите!</NavLink>
+                            </div>
+                        }
+                        <Button className="w-25 me-3" variant={"outline-success"}>
+                            {isLogin ? 'Войти' : 'Регистрация'}
+                        </Button>
+                    </Row>
+                </Form>
+            </Card>
+        </Container>
     );
 };
 
