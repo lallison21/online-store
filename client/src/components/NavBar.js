557b793d03a74ce952953e07bc1828c157fad90a
lallison21 <lallison@student.21-school.ru>
2022-09-16 17:37:11 +0300

    admin page, modal windows

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,20 +3,33 @@ import {Context} from "../index";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import {NavLink} from "react-router-dom";
-import {SHOP_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
 import {Button} from "react-bootstrap";
 import {observer} from "mobx-react-lite";
+import {useNavigate} from "react-router-dom";
 
 const NavBar = observer(() => {
     const {user} = useContext(Context)
+    const navigate = useNavigate()
 
     return (
         <Navbar bg="dark" variant="dark">
             <NavLink className="ms-5" style={{color: 'white', textDecoration: "none"}} to={SHOP_ROUTE}>BananaShop</NavLink>
             {user.isAuth ?
                 <Nav className="ms-auto me-5" style={{color: 'white'}}>
-                    <Button variant={"outline-light"}>Админ панель</Button>
-                    <Button variant={"outline-light"} className='mx-2'>Выйти</Button>
+                    <Button
+                        variant={"outline-light"}
+                        onClick={() => navigate(`${ADMIN_ROUTE}`)}
+                    >
+                        Админ панель
+                    </Button>
+                    <Button
+                        variant={"outline-light"}
+                        onClick={() => navigate(`${LOGIN_ROUTE}`)}
+                        className='mx-2'
+                    >
+                        Выйти
+                    </Button>
                 </Nav>
                 :
                 <Nav className="ms-auto me-5" style={{color: 'white'}}>
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Button, Container} from "react-bootstrap";
+import CreateBrand from "../components/modals/CreateBrand";
+import CreateItem from "../components/modals/CreateItem";
+import CreateType from "../components/modals/CreateType";
 
 const Admin = () => {
+    const [brandVisible, setBrandVisible] = useState(false)
+    const [typeVisible, setTypeVisible] = useState(false)
+    const [itemVisible, setItemVisible] = useState(false)
+
     return (
-        <div>
-            AdminPage
-        </div>
+        <Container className="d-flex flex-column">
+            <Button
+                variant={"outline-dark"}
+                className="mt-4 p-2"
+                onClick={() => setTypeVisible(true)}
+            >
+                Добавить новый тип
+            </Button>
+            <Button
+                variant={"outline-dark"}
+                className="mt-4 p-2"
+                onClick={() => setBrandVisible(true)}
+            >
+                Добавить новый бренд
+            </Button>
+            <Button
+                variant={"outline-dark"}
+                className="mt-4 p-2"
+                onClick={() => setItemVisible(true)}
+            >
+                Добавить новую одежду
+            </Button>
+            <CreateBrand show={brandVisible} onHide={() => setBrandVisible(false)}/>
+            <CreateItem show={itemVisible} onHide={() => setItemVisible(false)}/>
+            <CreateType show={typeVisible} onHide={() => setTypeVisible(false)}/>
+        </Container>
     );
 };
 
