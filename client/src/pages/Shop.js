3a37a31e2b026cbbeae4787e2eb7be493012d6f0
lallison21 <lallison@student.21-school.ru>
2022-09-16 18:22:18 +0300

    add banner on main pages and begin axios

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {Col, Container} from "react-bootstrap";
+import {Col, Container, Image} from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import TypeBar from "../components/TypeBar";
 import BrandBar from "../components/BrandBar";
 import ItemList from "../components/ItemList";
+import banner from "../assets/banner.jpg"
 
 const Shop = () => {
     return (
         <Container>
+            <Image className="mt-2" width={1300} height={400} src={banner} />
             <Row className="mt-3">
                 <Col md={3}>
                     <TypeBar/>
