bda48584f3ad2f60825f67805aa9124304eb9c6e
Kamil Kamalov <kamil17121998@gmail.com>
2022-09-22 13:28:29 +0300

    user registration only

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,7 +3,7 @@ import {Context} from "../index";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import {NavLink} from "react-router-dom";
-import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, BASKET_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
 import {Button} from "react-bootstrap";
 import {observer} from "mobx-react-lite";
 import {useNavigate} from "react-router-dom";
@@ -23,12 +23,21 @@ const NavBar = observer(() => {
             <NavLink className="ms-5" style={{color: 'white', textDecoration: "none"}} to={SHOP_ROUTE}>BananaShop</NavLink>
             {user.isAuth ?
                 <Nav className="ms-auto me-5" style={{color: 'white'}}>
-                    <Button
-                        variant={"outline-light"}
-                        onClick={() => navigate(`${ADMIN_ROUTE}`)}
-                    >
-                        Админ панель
-                    </Button>
+                    {user.user.role === 'ADMIN' ?
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => navigate(`${ADMIN_ROUTE}`)}
+                        >
+                            Админ панель
+                        </Button>
+                        :
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => navigate(`${BASKET_ROUTE}`)}
+                        >
+                            Корзина
+                        </Button>
+                    }
                     <Button
                         variant={"outline-light"}
                         onClick={() => logOut()}
