2faf3a98e1e871b536cd333b475530bfc4844630
Kamil Kamalov <kamil17121998@gmail.com>
2022-09-28 15:35:34 +0300

    Revision of the shopping cart page

--- client/src/pages/Basket.js
+++ client/src/pages/Basket.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, Col, Container} from "react-bootstrap";
+import {Button, Card, Col, Container} from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 
 const Basket = () => {
@@ -7,18 +7,20 @@ const Basket = () => {
         <Container>
             <h1 className="text-lg-center mt-3">КОРЗИНА ТОВАРОВ</h1>
             <hr/>
-            <Row>
-                <Col md={8}>
-                    <Card>
-                        Товары
+                <Col>
+                    <Card style={{minHeight: 500}}>
+                        <Row className="mt-3">
+                            <Col md={1} className="ms-4">Кол-во</Col>
+                            <Col md={9}>Наименование товара</Col>
+                            <Col md={1} className="ms-5">Стоимость</Col>
+                        </Row>
+                        <hr/>
+                        <h3 className="text-lg-center mt-5 mb-5">Товары в корзине отсутствуют</h3>
+                        <hr className="mt-auto"/>
+                        <h5 className="ms-auto mb-3 me-3">Общая стоимость: 0 руб.</h5>
+                        <Button variant={"outline-success"}>Оплатить товар</Button>
                     </Card>
                 </Col>
-                <Col md={4}>
-                    <Card>
-                        Их стоимость
-                    </Card>
-                </Col>
-            </Row>
         </Container>
     );
 };
